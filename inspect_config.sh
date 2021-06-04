924b91994f87b9b918939be278117ce59e5a5ba5
Anon <No email>
2021-06-04 07:06:42 +0000

    Add build scripts

---
+++ inspect_config.sh
@@ -0,0 +1,8 @@
+export PATH="$HOME/google_armv7_gcc/bin:$PATH"
+export ARCH=arm
+export CROSS_COMPILE=arm-linux-androideabi-
+
+mkdir out
+make O=out -j$(nproc) bq2400l_defconfig
+make O=out -j$(nproc) menuconfig
+
